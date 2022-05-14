f0d916a4fe547e0d55fa04d7eccdc3d2b6eea364
christine-nkatha <nkathachristine456@gmail.com>
2022-05-15 00:01:23 +0300

    migations

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, SubmitField
-# from wtforms.validators import Required
+from wtforms.validators import Datarequired
 
 
 class PostForm(FlaskForm):
